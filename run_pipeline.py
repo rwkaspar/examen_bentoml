d033b27339fdb132b96450200d4449bb6223074e
rwkaspar <kasparrobert@gmail.com>
2025-05-20 22:11:47 +0000

    added pipeline

---
+++ run_pipeline.py
@@ -0,0 +1,17 @@
+import os
+
+os.environ["PYTHONPATH"] = "src"
+
+steps = [
+    f"python src/import_data.py",
+    f"python src/prepare_data.py",
+    f"python src/train_model.py"
+]
+
+for step in steps:
+    print(f"\n Start: {step}")
+    if os.system(step) != 0:
+        print(f" Error at: {step}")
+        break
+    else:
+        print(f" Finished: {step}")
